e5312659a662fa65c79dcde7249e4d0386b06c89
Андрей <lau47@mail.ru>
2025-10-21 09:49:56 +0300

    Добавьте файлы проекта.

---
+++ SocialNetworkWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SocialNetworkWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
